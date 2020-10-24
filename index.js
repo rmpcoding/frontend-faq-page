9dc1c073f67b9414c158b4852a419b0987de7385
Robert Parsons <rmpcoding@gmail.com>
2020-10-23 22:31:33 -0700

    Adds click event listener to arrow icons

---
+++ index.js
@@ -0,0 +1,7 @@
+const arrowIcons = document.querySelectorAll('.arrow-icon');
+
+arrowIcons.forEach((arrowIcon) => {
+    arrowIcon.addEventListener('click', () => {
+        console.log('helloooooow!');
+    });
+});
