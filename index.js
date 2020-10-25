35853c2085b651b18fde86e8fb26cb529bad8f27
Robert Parsons <rmpcoding@gmail.com>
2020-10-25 00:24:23 -0700

    Adds targeted bold font to question when arrow icon is clicked

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const arrowIcons = document.querySelectorAll('.arrow-icon');
+const questions = document.querySelectorAll('.p__question');
 
 arrowIcons.forEach((arrowIcon) => {
     arrowIcon.addEventListener('click', () => {
-        console.log('helloooooow!');
+        arrowIcon.nextElementSibling.style.fontWeight = "700";
     });
 });
