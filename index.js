d14c55d636045a1ca9bd735f7d4fd9e8c2e0213f
Robert Parsons <rmpcoding@gmail.com>
2020-10-25 00:28:18 -0700

    Adds pseudocode logic for toggling on fontWeight

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ const questions = document.querySelectorAll('.p__question');
 arrowIcons.forEach((arrowIcon) => {
     arrowIcon.addEventListener('click', () => {
         arrowIcon.nextElementSibling.style.fontWeight = "700";
+        // Run ternary operation OR if statement to toggle on or off
+        // If fontWeight is equal to 700
+            // Change fontWeight to normal size
+        // Else do change fontWeight to 700
+        console.log(arrowIcon.nextElementSibling.style.fontWeight)
     });
 });
