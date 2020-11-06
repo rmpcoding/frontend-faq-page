588bcc3c63d26e8ee67e260e232a1b96c80af1f7
Robert Parsons <rmpcoding@gmail.com>
2020-11-05 19:02:14 -0800

    Adds jQuery CDN and functions for animated dropdown

--- index.js
+++ index.js
@@ -5,45 +5,36 @@ const answers = document.querySelectorAll('.p__answer');
 arrowIcons.forEach((arrowIcon) => {
     arrowIcon.addEventListener('click', () => {
         let isQuestionBold = arrowIcon.nextElementSibling.style.fontWeight;
-        
+
         // isQuestionBold initially starts off as falsy on first click
         if (!isQuestionBold) {
-            arrowIcon.nextElementSibling.style.fontWeight = "700";
-            answers[arrowIcon.getAttribute('data-id')].style.color = "hsl(240, 6%, 50%)";
-            answers[arrowIcon.getAttribute('data-id')].style.display = "block";
+            arrowIcon.nextElementSibling.style.fontWeight = '700';
+            answers[arrowIcon.getAttribute('data-id')].style.color =
+                'hsl(240, 6%, 50%)';
+            answers[arrowIcon.getAttribute('data-id')].style.display = 'block';
         }
 
         // toggles emboldened question to normal font weight
-        if (isQuestionBold === "700") {
-            arrowIcon.nextElementSibling.style.fontWeight = "400";
-            answers[arrowIcon.getAttribute('data-id')].style.display = "none";
+        if (isQuestionBold === '700') {
+            arrowIcon.nextElementSibling.style.fontWeight = '400';
+            answers[arrowIcon.getAttribute('data-id')].style.display = 'none';
         }
 
         // toggles normal font weight to emboldened question
-        if (isQuestionBold === "400") {
-            arrowIcon.nextElementSibling.style.fontWeight = "700";
-            answers[arrowIcon.getAttribute('data-id')].style.display = "block";
+        if (isQuestionBold === '400') {
+            arrowIcon.nextElementSibling.style.fontWeight = '700';
+            answers[arrowIcon.getAttribute('data-id')].style.display = 'block';
         }
-        
     });
 });
 
+// jQuery added for animations
+$(document).ready(() => {
+    arrowIcons.forEach((arrowIcon) => {
+        $('.arrow-icon').on('click', () => {
+            const answer = $(arrowIcon).siblings('.answer');
+            answer.children('.p__answer').slideDown(1000);
+        })
 
-
-// let isQuestionBold = arrowIcon.nextElementSibling.style.fontWeight;
-        
-// // isQuestionBold initially starts off as falsy on first click
-// if (!isQuestionBold) {
-//     arrowIcon.nextElementSibling.style.fontWeight = "700";
-//     answers[arrowIcon.getAttribute('data-id')].style.display = "block";
-// }
-
-// // toggles emboldened question to normal font weight
-// if (isQuestionBold === "700") {
-//     arrowIcon.nextElementSibling.style.fontWeight = "400";
-// }
-
-// // toggles normal font weight to emboldened question
-// if (isQuestionBold === "400") {
-//     arrowIcon.nextElementSibling.style.fontWeight = "700";
-// }
+    });
+});
