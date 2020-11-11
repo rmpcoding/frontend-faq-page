4874dd0b6b0476de64566a4eedd37f6c0271e838
Robert Parsons <rmpcoding@gmail.com>
2020-11-10 19:31:03 -0800

    Adds jQuery animation on dropdown menu item on arrow click

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const arrowIcons = document.querySelectorAll('.arrow-icon');
 const questions = document.querySelectorAll('.p__question');
 const answers = document.querySelectorAll('.p__answer');
 
+$(document).ready(() => {
+    
 arrowIcons.forEach((arrowIcon) => {
     arrowIcon.addEventListener('click', () => {
         let isQuestionBold = arrowIcon.nextElementSibling.style.fontWeight;
+        // let arrowId = answers[arrowIcon.getAttribute('data-id')].getAttribute('data-id')
+        let p__answer = answers[arrowIcon.getAttribute('data-id')]
+
+        $(p__answer).slideToggle('1000')
 
         // isQuestionBold initially starts off as falsy on first click
         if (!isQuestionBold) {
@@ -13,13 +19,11 @@ arrowIcons.forEach((arrowIcon) => {
                 'hsl(240, 6%, 50%)';
             answers[arrowIcon.getAttribute('data-id')].style.display = 'block';
         }
-
         // toggles emboldened question to normal font weight
         if (isQuestionBold === '700') {
             arrowIcon.nextElementSibling.style.fontWeight = '400';
-            answers[arrowIcon.getAttribute('data-id')].style.display = 'none';
+            answers[arrowIcon.getAttribute('data-id')].style.display = 'none';            
         }
-
         // toggles normal font weight to emboldened question
         if (isQuestionBold === '400') {
             arrowIcon.nextElementSibling.style.fontWeight = '700';
@@ -28,13 +32,9 @@ arrowIcons.forEach((arrowIcon) => {
     });
 });
 
-// jQuery added for animations
-$(document).ready(() => {
-    arrowIcons.forEach((arrowIcon) => {
-        $('.arrow-icon').on('click', () => {
-            const answer = $(arrowIcon).siblings('.answer');
-            answer.children('.p__answer').slideDown(1000);
-        })
-
-    });
 });
+
+
+
+
+// jQuery added for animations
