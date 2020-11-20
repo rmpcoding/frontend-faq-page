451a9752e168bb87fd4ef6d16ad9f9182980ea25
Robert Parsons <rmpcoding@gmail.com>
2020-11-19 21:19:22 -0800

    Adds hidden overflow to image, but image is not stable at the moment

--- index.js
+++ index.js
@@ -3,38 +3,36 @@ const questions = document.querySelectorAll('.p__question');
 const answers = document.querySelectorAll('.p__answer');
 
 $(document).ready(() => {
-    
-arrowIcons.forEach((arrowIcon) => {
-    arrowIcon.addEventListener('click', () => {
-        let isQuestionBold = arrowIcon.nextElementSibling.style.fontWeight;
-        // let arrowId = answers[arrowIcon.getAttribute('data-id')].getAttribute('data-id')
-        let p__answer = answers[arrowIcon.getAttribute('data-id')]
+    arrowIcons.forEach((arrowIcon) => {
+        arrowIcon.addEventListener('click', () => {
+            let isQuestionBold = arrowIcon.nextElementSibling.style.fontWeight;
+            // let arrowId = answers[arrowIcon.getAttribute('data-id')].getAttribute('data-id')
+            let p__answer = answers[arrowIcon.getAttribute('data-id')];
 
-        $(p__answer).slideToggle('1000')
+            $(p__answer).slideDown('1000');
 
-        // isQuestionBold initially starts off as falsy on first click
-        if (!isQuestionBold) {
-            arrowIcon.nextElementSibling.style.fontWeight = '700';
-            answers[arrowIcon.getAttribute('data-id')].style.color =
-                'hsl(240, 6%, 50%)';
-            answers[arrowIcon.getAttribute('data-id')].style.display = 'block';
-        }
-        // toggles emboldened question to normal font weight
-        if (isQuestionBold === '700') {
-            arrowIcon.nextElementSibling.style.fontWeight = '400';
-            answers[arrowIcon.getAttribute('data-id')].style.display = 'none';            
-        }
-        // toggles normal font weight to emboldened question
-        if (isQuestionBold === '400') {
-            arrowIcon.nextElementSibling.style.fontWeight = '700';
-            answers[arrowIcon.getAttribute('data-id')].style.display = 'block';
-        }
+            // isQuestionBold initially starts off as falsy on first click
+            if (!isQuestionBold) {
+                arrowIcon.nextElementSibling.style.fontWeight = '700';
+                answers[arrowIcon.getAttribute('data-id')].style.color =
+                    'hsl(240, 6%, 50%)';
+                answers[arrowIcon.getAttribute('data-id')].style.display =
+                    'block';
+            }
+            // toggles emboldened question to normal font weight
+            if (isQuestionBold === '700') {
+                arrowIcon.nextElementSibling.style.fontWeight = '400';
+                answers[arrowIcon.getAttribute('data-id')].style.display =
+                    'none';
+            }
+            // toggles normal font weight to emboldened question
+            if (isQuestionBold === '400') {
+                arrowIcon.nextElementSibling.style.fontWeight = '700';
+                answers[arrowIcon.getAttribute('data-id')].style.display =
+                    'block';
+            }
+        });
     });
 });
-
-});
-
-
-
 
 // jQuery added for animations
